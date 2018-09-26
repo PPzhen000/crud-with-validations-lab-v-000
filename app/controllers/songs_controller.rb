e1067e921dfc09b8a111f45fe28768dba685a33d
Ariel Zhen <zhenpingping@live.com>
2018-09-26 20:36:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,13 @@ class SongsController < ApplicationController
   end
 
   def create
+    @song = Song.new(song_params)
 
+    if @song.save 
+      redirect_to song_path(@song)
+    else 
+      render :new 
+    end 
   end
 
   def update
